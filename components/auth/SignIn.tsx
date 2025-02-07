b22062e081d3e2e3e98c37c84ffc18db76d9b5c7
tikajeremias <tikajeremias@gmail.com>
2025-02-07 16:01:29 -0300

    Styling Dumbbell SignIn and SignUp

--- components/auth/SignIn.tsx
+++ components/auth/SignIn.tsx
@@ -6,11 +6,14 @@ import { supabase } from '@/lib/supabase';
 import { textStyles } from '@/style/textStyles';
 import { buttonStyles } from '@/style/buttonStyles';
 import { inputStyles } from '@/style/inputStyles';
-import HeroImage from '../HeroImage';
+import { useAssets } from 'expo-asset';
 
 export default function SignIn({ setSignIn }: { setSignIn: (value: boolean) => void }) {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [assets, error] = useAssets([
+        require('@/assets/images/heroDumbbell.png'),
+    ]);
 
     const handleSignIn = async () => {
         const { error } = await supabase.auth.signInWithPassword({ email, password });
@@ -28,46 +31,54 @@ export default function SignIn({ setSignIn }: { setSignIn: (value: boolean) => v
     return (
         <View style={commonStyles.container}>
 
-            {/* <HeroImage /> */}
-            {/* <Image source={}/> */}
+            <View style={commonStyles.containerBetween}>
+                <View style={commonStyles.content}>
+                    {/* @ts-ignore */}
+                    <Image source={assets?.[0]} />
 
-            <Text style={textStyles.titleLg}>SignIn</Text>
+                    <Text style={textStyles.titleLg}>Dumbbell</Text>
 
-            <TextInput
-                style={inputStyles.input}
-                placeholder="Correo electrónico"
-                value={email}
-                onChangeText={setEmail}
-                autoCapitalize="none"
-                placeholderTextColor='#DBD6C9'
-            />
-            <TextInput
-                style={inputStyles.input}
-                placeholder="Contraseña"
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry
-                placeholderTextColor='#DBD6C9'
-            />
-
-            <TouchableOpacity style={buttonStyles.button} onPress={handleSignIn}>
-                <Text style={textStyles.textMd}>Iniciar Sesión</Text>
-            </TouchableOpacity>
+                    <TextInput
+                        style={inputStyles.input}
+                        placeholder="Correo electrónico"
+                        value={email}
+                        onChangeText={setEmail}
+                        autoCapitalize="none"
+                        placeholderTextColor='#DBD6C9'
+                    />
+                    <TextInput
+                        style={inputStyles.input}
+                        placeholder="Contraseña"
+                        value={password}
+                        onChangeText={setPassword}
+                        secureTextEntry
+                        placeholderTextColor='#DBD6C9'
+                    />
 
 
-            <TouchableOpacity>
-                <Text style={textStyles.span}>Olvide mi contraseña. Recuperar.</Text>
-            </TouchableOpacity>
 
-            {/* <Text style={textStyles.span}>O iniciar con</Text> */}
+                    <View style={commonStyles.containerEnd}>
+                        <Text style={textStyles.span}>Olvide mi contraseña. Recuperar.</Text>
+                    </View>
 
-            {/* <TouchableOpacity style={commonStyles.button} onPress={handleSignInWithGoogle}>
+                    {/* <Text style={textStyles.span}>O iniciar con</Text> */}
+
+                    {/* <TouchableOpacity style={commonStyles.button} onPress={handleSignInWithGoogle}>
                 <Text style={commonStyles.buttonText}>Google</Text>
-            </TouchableOpacity> */}
+                </TouchableOpacity> */}
 
-            <TouchableOpacity onPress={handleToggleSignUp}>
-                <Text style={textStyles.span}>¿No tienes una cuenta? Crear cuenta.</Text>
-            </TouchableOpacity>
+
+                </View>
+
+                <View style={commonStyles.containerEnd}>
+                    <Text onPress={handleToggleSignUp} style={textStyles.span}>¿No tienes una cuenta? Crear cuenta.</Text>
+                </View>
+
+                <TouchableOpacity style={buttonStyles.button} onPress={handleSignIn}>
+                    <Text style={textStyles.buttonText}>Iniciar Sesión</Text>
+                </TouchableOpacity>
+
+            </View>
         </View>
     );
 }
--- style/buttonStyles.ts
+++ style/buttonStyles.ts
@@ -15,6 +15,22 @@ export const buttonStyles = StyleSheet.create({
         height: 52,
         textAlign: "center",
     },
+    buttonUI: {
+        backgroundColor: colors.sg2,
+        borderRadius: 5,
+        color: colors.sg2,
+        borderColor: colors.sg5,
+        display: "flex",
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderWidth: 1,
+        width: '100%',
+        height: 52,
+        textAlign: "center",
+        position: 'absolute',
+        bottom: 0,
+
+    },
     card: {
         color: colors.sg2,
         fontWeight: 'normal',
