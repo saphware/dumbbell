36f59137efa8dffbd9ccc4ce44929b0f1b83a1cb
tikajeremias <tikajeremias@gmail.com>
2025-03-05 02:41:12 -0300

    [DMB-37] Recipe

--- app/(app)/recipes/recipe/[id].tsx
+++ app/(app)/recipes/recipe/[id].tsx
@@ -7,17 +7,20 @@ import { textStyles } from '@/style/textStyles';
 import { useRecipes } from '@/hooks/useRecipes';
 import { Image } from 'expo-image';
 import Markdown from 'react-native-markdown-display';
+import React from 'react';
 
 export default function recipe() {
 
   const { id } = useLocalSearchParams();
   const { getRecipeById } = useRecipes();
   const [recipe, setRecipe] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchRecipe = async () => {
       const fetchedRecipe = await getRecipeById(Number(id));
       setRecipe(fetchedRecipe);
+      setLoading(false); // Set loading to false after fetching
     };
     fetchRecipe();
   }, [id]);
@@ -32,28 +35,38 @@ export default function recipe() {
 
   return (
     <SafeAreaProvider style={commonStyles.containerNavbars}>
-
-      {recipe ? (
-        <ScrollView showsVerticalScrollIndicator={false}>
-
-          {/* @ts-ignore */}
-          <Text style={textStyles.titleMd}>{recipe.title}</Text>
-
-          <Image
-            style={commonStyles.recipeImage}
-            source={{
-              // @ts-ignore
-              uri: recipe.image_url,
-            }}
-          />
-
-          {/* @ts-ignore */}
-          <Text style={textStyles.textLg}>{recipe.description}</Text>
-          {/* <MarkdownText content={recipe.description} /> */}
-
-        </ScrollView>
+      {loading ? (
+        <Text style={textStyles.textLg}>
+          Loading...
+        </Text>
       ) : (
-        <Text style={textStyles.textMd}>Recipe not found</Text>
+        <>
+          {
+            recipe ? (
+              <ScrollView showsVerticalScrollIndicator={false}>
+
+                {/* @ts-ignore */}
+                <Text style={textStyles.titleMd}>{recipe.title}</Text>
+
+                <Image
+                  style={commonStyles.recipeImage}
+                  source={{
+                    // @ts-ignore
+                    uri: recipe.image_url,
+                  }}
+                />
+
+                {/* @ts-ignore */}
+                <Text style={textStyles.textLg}>{recipe.description}</Text>
+                {/* <MarkdownText content={recipe.description} /> */}
+
+              </ScrollView>
+            ) : (
+              <Text style={textStyles.textMd}>Recipe not found</Text>
+            )
+          }
+
+        </>
       )}
 
     </SafeAreaProvider>
