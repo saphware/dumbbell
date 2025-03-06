e6e2db7a576d6fb7633c6d561b2aa82f27b3db34
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-06 01:00:56 -0300

    Se modifica archivo types y se crea carpeta de tipos

---
+++ types/coach.ts
@@ -0,0 +1,8 @@
+export type Coach = {
+    id: number;
+    id_user: number;
+    user_image: string;
+    description: string;
+    created_at: string;
+    updated_at: string;
+};
---
+++ types/routine.ts
@@ -0,0 +1,10 @@
+export type Routine = {
+    id: number;
+    id_client: number;
+    id_coach: number;
+    name: string;
+    goal: string;
+    created_at: string;
+    updated: boolean;
+    last_updated: string;
+};
