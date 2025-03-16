657fe4a708d3ba3f3058f2810895c96229dbc27a
FrancoSecchi <francofabiansecchi@gmail.com>
2025-03-16 18:53:03 -0300

    usando getUser

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,13 +1,14 @@
 import { SafeAreaView } from "react-native";
 import { useState } from "react";
 import { commonStyles } from "@/style/commonStyles";
+import { getUser } from "@/hooks/useProfile";
 import SignIn from "@/components/auth/SignIn";
 import SignUp from "@/components/auth/SignUp";
 
 export default function Auth() {
 
     const [signIn, setSignIn] = useState(true)
-
+    getUser();
     return (
         <SafeAreaView style={commonStyles.mainContainer}>
             {
